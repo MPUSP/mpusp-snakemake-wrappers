f3b36a3da7b98b4bfa24629f612e2af64df05700
m-jahn <jahn@mpusp.mpg.de>
2025-05-28 08:43:41 +0200

    fix: added 1 optional input and improved input handling

--- pycoqc/wrapper.py
+++ pycoqc/wrapper.py
@@ -14,15 +14,23 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 # get input files
 summary = snakemake.input.get("summary")
 bam = snakemake.input.get("bam", "")
+barcode = snakemake.input.get("barcode", "")
 
 
 # prepare input
-def concatenate_input(files):
-    return " ".join(files) if isinstance(files, list) else files
+def concatenate_input(prefix, files):
+    if files:
+        files = " ".join(files) if isinstance(files, list) else files
+        return f"--{prefix} " + files
+    else:
+        if prefix == "summary_file":
+            raise ValueError("'summary' input is required for pycoQC.")
+        return ""
 
 
-summary = concatenate_input(summary)
-bam = concatenate_input(bam) if bam else ""
+summary = concatenate_input("summary_file", summary)
+bam = concatenate_input("bam_file", bam)
+barcode = concatenate_input("barcode_file", barcode)
 
 # prepare output
 report_html = snakemake.output.get("report_html")
@@ -31,8 +39,9 @@ report_json = snakemake.output.get("report_json")
 # run pycioQC
 shell(
     "pycoQC"
-    " -f {summary}"  # required summary input
+    " {summary}"  # required summary input
     " {bam}"  # optional bam input
+    " {barcode}"  # optional barcode input
     " {extra}"  # additional options
     " -o {report_html}"  # html output
     " -j {report_json}"  # json output
